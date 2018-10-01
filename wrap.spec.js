3503cb15a3c6769bc5303b3655d468ef8014880a
Dalton Saffe <daltonsaffe@gmail.com>
2018-10-01 11:43:18 -0400

    test one written

--- wrap.spec.js
+++ wrap.spec.js
@@ -5,4 +5,20 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap('', 10)).to.equal('');
   });
+  it('If column number is less than the character count it just returns the string', () => {
+    expect(wrap('Hey', 12)).to.equal('Hey');
+  });
+  it('Returns a string broken up at every second position', () => {
+    expect(wrap('Lorem', 2)).to.equal('Lo\nre\nm');
+  });
+  it('Returns a string broken up per word at each column', () => {
+    expect(
+      wrap(
+        'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.',
+        20
+      )
+    ).to.equal(
+      'Lorem ipsum dolor\nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula.'
+    );
+  });
 });
